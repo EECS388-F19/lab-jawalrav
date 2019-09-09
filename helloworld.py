ed374f08019dbe3097a3581c46f6a1ff9e75d43e
Jacob A Walraven <jawalrav@beyster1620p04.engin.umich.edu>
2019-09-09 15:40:20 -0400

    woo

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from statistics import mean
+import random
+print("Jacob")
+num = random.randint(0,100)
+num2 = random.randint(0,100)
+
+print(num)
+print(num2)
+print("Sum = ", num + num2)
+print("Average = ", mean([num, num2]))
